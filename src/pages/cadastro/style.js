00dd36a286495612e3a1f12836f86ae9d33b1b35
Roberto Barrosss <robertobarros27esp@gmail.com>
2022-07-18 20:05:23 -0300

    Create style.js

---
+++ src/pages/cadastro/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    input: {
+        borderBottomWidth: 1,
+        borderBottomColor: '#313131',
+        marginTop: 10,
+        width: '80%',
+        height: 50
+    }
+})
+
+export default styles
