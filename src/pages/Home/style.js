eb9fb7f7fc965dae7e01060f5a8129ca45b4d3f9
Roberto Barrosss <robertobarros27esp@gmail.com>
2022-07-18 20:07:12 -0300

    Create style.js

---
+++ src/pages/Home/style.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    home: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center', 
+      marginTop: 30,
+      button: 'black',
+    },
+
+    loginText: {
+      marginTop: 16,
+      paddingVertical: 4,
+     // borderWidth: 4,
+     // borderColor: "#20232a",
+      borderRadius: 2,
+      backgroundColor: "#66CDAA",
+      color: "#FFFAFA",
+      textAlign: "center",
+      fontSize: 20,
+      fontWeight: "Arial",
+      width: '80%',      
+    },
+
+    homeText: {
+      fontSize: 20,
+      fontWeight: 'Arial',
+      color: '#808080',
+    }
+    
+});
+
+export default styles
