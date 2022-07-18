8f381fdc03baabc3e97353871e76d6932b3f3160
Roberto Barrosss <robertobarros27esp@gmail.com>
2022-07-18 20:05:11 -0300

    Create cadastro.js

---
+++ src/pages/cadastro/cadastro.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import {
+    View,
+    Text,
+    TextInput,
+    Button
+} from 'react-native';
+import styles from "../cadastro/style";
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../components/firebase';
+
+export function Cadastro({ navigation }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function createUser() {
+        await createUserWithEmailAndPassword(auth, email, password)     
+        .then(value => {
+            console.log('Cadastrado com sucesso! \n' + value.user.uid.email);
+            {
+              alert('Cadastro criado com sucesso');
+            }
+          })
+        .catch(error => console.log(error));
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text>FAZER CADASTRO</Text>
+            <TextInput 
+              placeholder="Email"
+              placeholderTextColor="#313131"
+              value={email}
+              onChangeText={value => setEmail(value)}
+              style={styles.input}
+            />
+            <TextInput 
+              placeholder="Senha"
+              placeholderTextColor="#313131"
+              value={password}
+              onChangeText={value => setPassword(value)}
+              style={styles.input}
+            />
+
+            <Button 
+              title="CADASTRAR"
+              onPress={() => createUser()}
+            />
+
+            <Text 
+              style={styles.loginText}
+              onPress={() => navigation.navigate('Login')}>
+              Fez o cadastro? Click aqui para logar
+            </Text> 
+        </View>
+    )
+}
+
+
+
+/*
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+
+const Cadastro = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Cadastre-se</Text>
+    <Button 
+      title="Ir a Home"
+      onPress={() => navigation.navigate('Home') }
+    />
+  </View>
+);
+
+Cadastro.navigationOptions = {
+  title: 'Cadastro',
+}
+
+export default Cadastro;
+*/
