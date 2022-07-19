7745f8a7193c2429569ce25c81c17487865043ec
Roberto Barrosss <robertobarros27esp@gmail.com>
2022-07-18 22:31:47 -0300

    Update style.js

--- src/pages/cadastro/style.js
+++ src/pages/cadastro/style.js
@@ -11,8 +11,34 @@ const styles = StyleSheet.create({
         borderBottomColor: '#313131',
         marginTop: 10,
         width: '80%',
-        height: 50
-    }
+        height: 50,
+        fontSize: 20,
+        fontWeight: "Arial",
+        color: '#808080'
+    },
+    button: {
+        marginTop: 16,
+        paddingVertical: 4,
+        borderRadius: 2,
+        backgroundColor: "#66CDAA",
+        color: "#FFFAFA",
+        textAlign: "center",
+        fontSize: 20,
+        fontWeight: "Arial",
+        width: 290 
+    },
+    cadastroText: {
+        fontSize: 20,
+        fontWeight: 'Arial',
+        color: '#808080',
+      },
+      loginText: {
+        fontSize: 13,
+        fontWeight: 'Arial',
+        color: '#808080',
+        alignItems: 'center',
+        marginTop: 15
+      }
 })
 
 export default styles
